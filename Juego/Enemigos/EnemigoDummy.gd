310adec4e52387290c20672fcea88a08b48ff6cd
BERZERKER1866 <maxiusi78@gmail.com>
2022-10-26 13:53:35 -0300

    Update EnemigoDummy.gd

--- Juego/Enemigos/EnemigoDummy.gd
+++ Juego/Enemigos/EnemigoDummy.gd
@@ -1,8 +1,18 @@
 ## EnemigoDummy.gd
 extends Node2D
 
+## Atributos
+var hitpoints : float = 10.0
 
-
+## Metodos
 func _on_Area2D_body_entered(body : Node) -> void:
 	if body is Player:
 		body.destruir()
+
+func recibir_danio(danio : float) -> void:
+	hitpoints -= danio
+	if hitpoints <= 0.0:
+		queue_free()
+
+func _process(delta : float) -> void:
+	$Canion.set_esta_disparando(true)
